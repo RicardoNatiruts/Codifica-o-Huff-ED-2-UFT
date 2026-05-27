d21dd2a7c64742bb51cb804e6791474d312bc965
tarcisof <tarcisof26@gmail.com>
2026-05-27 16:32:00 -0300

    Commite das structs

--- MinHeap.c
+++ MinHeap.c
@@ -1,1 +1,2 @@
+#include "MinHeap.h"
 
--- MinHeap.h
+++ MinHeap.h
@@ -1,1 +1,21 @@
+#ifndef MINHEAP_H
+#define MINHEAP_H
+#include <stdlib.h>
+#include <stdio.h>
 
+#define MAX_NOS 256
+
+typedef struct TNo {
+    unsigned char byte;
+    unsigned int frequencia;
+    struct TNo *esq; 
+    struct TNo *dir; 
+} TNo;
+
+typedef struct TreeHeap {
+    int tamanho;
+    TNo **root; 
+} TreeHeap;
+
+
+#endif
